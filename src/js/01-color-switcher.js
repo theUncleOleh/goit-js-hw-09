f00d5f7e107c9f30368aee3401e6192a60aa70bf
Oleh Semennykov <sumcue@gmail.com>
2021-11-06 19:26:15 +0200

    first task in work

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+    body : document.querySelector('body'),
+    startBtn : document.querySelector('button[data-start]'),
+    stopBtn : document.querySelector('button[data-stop]'),
+};
 
+let timerId = null;
+
+
+console.log(refs.startBtn);
+console.log(refs.stopBtn);
+
+refs.startBtn.addEventListener('click', handleStartBtnClick)
+refs.stopBtn.addEventListener('click', handleStopBtnClick)
+
+
+function handleStartBtnClick(evt) {
+ timerId = setInterval(() => {
+     refs.body.style.backgroundColor =  getRandomHexColor();
+     
+ }, 1000);
+ refs.startBtn.setAttribute("disabled", "disabled");
+};
+
+function handleStopBtnClick(evt) {
+    clearInterval(timerId);
+refs.startBtn.removeAttribute("disabled");
+};
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
